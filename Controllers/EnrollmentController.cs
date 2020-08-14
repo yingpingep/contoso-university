a62a8865e51320f35886fe97f9012887d38b5aed
yinnping <ciaoing@livemail.tw>
2020-08-15 00:13:04 +0800

    Update Enrollment controller

--- Controllers/EnrollmentController.cs
+++ Controllers/EnrollmentController.cs
@@ -1,10 +1,11 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using contoso_university.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace contoso_university.Controllers
 {
@@ -12,40 +13,106 @@ namespace contoso_university.Controllers
     [ApiController]
     public class EnrollmentController : ControllerBase
     {
-        public EnrollmentController()
+        private readonly ContosoUniversityContext _context;
+
+        public EnrollmentController(ContosoUniversityContext context)
         {
+            _context = context;
         }
 
-        // GET api/enrollment
+        // GET api/Enrollment
         [HttpGet("")]
         public ActionResult<IEnumerable<Enrollment>> GetEnrollments()
         {
-            return new List<Enrollment> { };
+            return _context.Enrollment.ToArray();
         }
 
-        // GET api/enrollment/5
+        // GET api/Enrollment/5
         [HttpGet("{id}")]
-        public ActionResult<Enrollment> GetEnrollmentById(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Enrollment>> GetEnrollmentById(int id)
         {
-            return null;
+            var Enrollment = await _context.Enrollment.FindAsync(id);
+
+            if (Enrollment == null) {
+                return NotFound();
+            }
+
+            return Enrollment;
         }
 
-        // POST api/enrollment
+        // POST api/Enrollment
         [HttpPost("")]
-        public void PostEnrollment(Enrollment value)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Enrollment>> PostEnrollment(Enrollment Enrollment)
         {
+            _context.Enrollment.Add(Enrollment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEnrollmentById", new { id = Enrollment.EnrollmentId }, Enrollment);
         }
 
-        // PUT api/enrollment/5
+        // PUT api/Enrollment/5
         [HttpPut("{id}")]
-        public void PutEnrollment(int id, Enrollment value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PutEnrollment(int id, Enrollment patchEnrollment)
         {
+            if (id != patchEnrollment.EnrollmentId) {
+                return BadRequest();
+            }
+
+            _context.Entry(patchEnrollment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                if (CheckEnrollmentExist(id)) {
+                    return NotFound();
+                }
+                throw;
+            }
+            return Ok();
         }
 
-        // DELETE api/enrollment/5
+        // DELETE api/Enrollment/5
         [HttpDelete("{id}")]
-        public void DeleteEnrollmentById(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeleteEnrollmentById(int id)
         {
+            var Enrollment = await _context.Enrollment.FindAsync(id);
+            if (Enrollment == null) {
+                return NotFound();
+            }
+
+            _context.Remove(Enrollment).State = EntityState.Deleted;
+            
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                if (CheckEnrollmentExist(id)) {
+                    return NotFound();
+                }
+                throw;
+            }
+            return Ok();
+        }
+
+        private bool CheckEnrollmentExist(int id) {
+            return _context.Enrollment.Any(Enrollment => Enrollment.EnrollmentId == id);
         }
     }
 }
