26b02845d1946126bfaa4bca891172e4314436e2
yinnping <ciaoing@livemail.tw>
2020-08-15 00:17:46 +0800

    Update person controller

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using contoso_university.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace contoso_university.Controllers
 {
@@ -11,40 +13,106 @@ namespace contoso_university.Controllers
     [ApiController]
     public class PersonController : ControllerBase
     {
-        public PersonController()
+        private readonly ContosoUniversityContext _context;
+
+        public PersonController(ContosoUniversityContext context)
         {
+            _context = context;
         }
 
-        // GET api/person
+        // GET api/Person
         [HttpGet("")]
         public ActionResult<IEnumerable<Person>> GetPersons()
         {
-            return new List<Person> { };
+            return _context.Person.ToArray();
         }
 
-        // GET api/person/5
+        // GET api/Person/5
         [HttpGet("{id}")]
-        public ActionResult<Person> GetPersonById(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Person>> GetPersonById(int id)
         {
-            return null;
+            var Person = await _context.Person.FindAsync(id);
+
+            if (Person == null) {
+                return NotFound();
+            }
+
+            return Person;
         }
 
-        // POST api/person
+        // POST api/Person
         [HttpPost("")]
-        public void PostPerson(Person value)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<Person>> PostPerson(Person Person)
         {
+            _context.Person.Add(Person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPersonById", new { id = Person.Id }, Person);
         }
 
-        // PUT api/person/5
+        // PUT api/Person/5
         [HttpPut("{id}")]
-        public void PutPerson(int id, Person value)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> PutPerson(int id, Person patchPerson)
         {
+            if (id != patchPerson.Id) {
+                return BadRequest();
+            }
+
+            _context.Entry(patchPerson).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                if (CheckPersonExist(id)) {
+                    return NotFound();
+                }
+                throw;
+            }
+            return Ok();
         }
 
-        // DELETE api/person/5
+        // DELETE api/Person/5
         [HttpDelete("{id}")]
-        public void DeletePersonById(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<IActionResult> DeletePersonById(int id)
         {
+            var Person = await _context.Person.FindAsync(id);
+            if (Person == null) {
+                return NotFound();
+            }
+
+            _context.Remove(Person).State = EntityState.Deleted;
+            
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (System.Exception)
+            {
+                if (CheckPersonExist(id)) {
+                    return NotFound();
+                }
+                throw;
+            }
+            return Ok();
+        }
+
+        private bool CheckPersonExist(int id) {
+            return _context.Person.Any(Person => Person.Id == id);
         }
     }
 }
